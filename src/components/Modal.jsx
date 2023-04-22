213dbc11f28a6bb33e10c1bb6c32967ed2385368
manaomimasha <manaomi.masha@gmail.com>
2023-04-22 15:44:03 -0300

    dkkdkdk

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,14 +4,19 @@ import CerrarBtn from '../img/cerrar.svg'
 const Modal = ({ setModal, animarmodal, setAnimarmodal }) => {
 
     const [nombre, setNombre] = useState("")
-
     const [cantidad, setCantidad] = useState("")
+    const [categoria, setCategoria] = useState("")
 
     const handleCerrar = () => {
         setAnimarmodal(false)
         setTimeout(() => {
             setModal(false)
         }, 500);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log("enviando formulario")
     }
 
     return (
@@ -27,7 +32,7 @@ const Modal = ({ setModal, animarmodal, setAnimarmodal }) => {
 
 
 
-            <form className={`formulario ${animarmodal ? "animar" : "cerrar "} `}>
+            <form onSubmit={handleSubmit} className={`formulario ${animarmodal ? "animar" : "cerrar "} `}>
 
                 <legend>NUEVO GASTO </legend>
 
@@ -55,18 +60,25 @@ const Modal = ({ setModal, animarmodal, setAnimarmodal }) => {
 
                 <div className='campo'  >
 
-                    <label htmlFor="cant"> Selecciona una categoría </label>
-                    <select name="" id="">
+                    <label htmlFor="categ"> Selecciona una categoría </label>
+                    <select name="" id="categ"
+                    
+                    value={categoria}
+                    onChange={ e => setCategoria(e.target.value)} >
 
                         <option value=""> --SELECIONA--  </option>
 
                         <option value="casa">CASA  </option>
 
-                        <option value="casa">OCIO  </option>
+                        <option value="ocio">OCIO  </option>
 
-                        <option value="casa">TRANSPORTE  </option>
+                        <option value="transporte">TRANSPORTE  </option>
 
-                        <option value="casa">SUSCRIPCIONES  </option>
+                        <option value="suscripciones">SUSCRIPCIONES  </option>
+                        
+                        <option value="comida">COMIDA  </option>
+                        
+                        <option value="hijos">HIJOS  </option>
                     </select>
 
                     <input type="submit" value="AÑADIR Gasto" />
